343a37caf82d0517b2a5d81e355d5463c71630a1
jenvyxu <1272117264@qq.com>
2019-10-17 23:43:37 +0800

    formate code

--- src/js/admin/event-hub.js
+++ src/js/admin/event-hub.js
@@ -1,22 +1,21 @@
-window.eventHub={
-    events:{
-    //'羊城晚报':[],
-    //'楚天都市报':[],
-    },
-    emit(eventName,data){//发布
-    for(let key in this.events){
-        if(key===eventName){
-            let fnList=this.events[key]
-            fnList.map((fn)=>{
-                fn.call(undefined,data)
-            })
-            }
-        }
-    },
-    on(eventName,fn){//订阅
-        if(this.events[eventName]===undefined){
-            this.events[eventName]=[]
-        }
-        this.events[eventName].push(fn)
-    },
-}
+window.eventHub = {
+  events: {},
+  emit(eventName, data) {
+    //发布
+    for (let key in this.events) {
+      if (key === eventName) {
+        let fnList = this.events[key];
+        fnList.map(fn => {
+          fn.call(undefined, data);
+        });
+      }
+    }
+  },
+  on(eventName, fn) {
+    //订阅
+    if (this.events[eventName] === undefined) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(fn);
+  }
+};
